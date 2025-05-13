016994b61101be16ba98b6cf1b3a5a71efe93c34
Md. Nazmul Islam Hridoy <waheddr1@gmail.com>
2025-05-13 08:46:29 +0600

    Add files via upload

---
+++ BCS BOOK/android/src/main/java/io/github/some_example_name/pdf1.java
@@ -0,0 +1,13 @@
+package io.github.some_example_name;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pdf1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf1);
+    }
+}
